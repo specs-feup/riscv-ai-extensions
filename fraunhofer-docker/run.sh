428f312b3826dd6b6348650340e95e896bfa5890
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2024-04-24 16:32:36 +0100

    Initial commit

---
+++ fraunhofer-docker/run.sh
@@ -0,0 +1,1 @@
+docker run -it --name airisc airisc:latest /bin/bash -c "echo 'Container Started'; /bin/bash"
---
+++ rnnasip-docker/run.sh
@@ -0,0 +1,1 @@
+docker run -it --name rnnasip rnnasip:latest /bin/bash -c "echo 'Container Started'; /bin/bash"
