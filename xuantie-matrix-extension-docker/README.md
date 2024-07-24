9fb6d42508020f58b5c3ac2d13a3d0b0af0aa34d
Antarex <>
2024-07-24 16:57:06 +0100

    Initial steps for xuantie matrix extension.

---
+++ xuantie-matrix-extension-docker/README.md
@@ -0,0 +1,15 @@
+# Xuantie's Matrix Extension
+
+TODO
+
+## Status
+
+- Under construction...
+
+## Matrix Extension Information
+
+TODO
+
+## Building Observations
+
+TODO
---
+++ xuantie-matrix-extension-docker/run.sh
@@ -0,0 +1,12 @@
+CONTAINER_NAME=xuantie-matrix-extension
+
+if [ "$(docker ps -a -q -f name=^${CONTAINER_NAME}$)" ]; then
+  echo "Container '$CONTAINER_NAME' exists. Removing..."
+  docker rm $CONTAINER_NAME
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to remove the container '$CONTAINER_NAME'."
+  fi
+fi
+
+docker run -it --name $CONTAINER_NAME $CONTAINER_NAME:latest /bin/bash -c "echo 'Container Started'; /bin/bash"
